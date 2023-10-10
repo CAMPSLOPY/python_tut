be70d39e4fc665aef4fa0ad3869032b700bef0c6
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-10-10 15:15:15 +0200

    more day 4

--- variables.py
+++ variables.py
@@ -318,3 +318,27 @@ if(company.__contains__(state)):
     print(f"{state} is avalable in the statement")
 else:
     print(f"{state} not available in the statement")
+
+company_3 = company.replace("Coding", "Python")
+company_4 = company_3.replace("All", "Everyone")
+tech = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+tech_split = tech.split(",")
+
+split_statement = company.split(" ")
+com_index =company[0]
+com_last_index =company[-1]
+com_tenth_index =company[10] #the index at the 10th value is space
+# lets create an acronym from the "python for all"
+
+# step 1 
+#  split the statement into individual elements
+
+# step 2 Iterate through each words to get the position of the first letter in each words index
+
+acronym = []
+words = company_3.split()
+
+for word in words:
+    acronym +=word[0]
+
+print(acronym)
