65f275b44aa2324d0547340b3d9cea23e0638e28
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-10-30 15:56:19 +0100

    lists

---
+++ day5_list.py
@@ -0,0 +1,19 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4] # it returns all the fruits
+# this will also give the same result as the one above
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3] # it does not include the first index
+orange_mango_lemon = fruits[1:]
+orange_and_lemon = fruits[::2] 
+
+
+list1 = ['item1', 'item2']
+list2 = ['item3', 'item4', 'item5']
+list1.extend(list2)
+
+print(list1) 
+
+
+empty = ['banana', 'orange', 'mango', 'lemon','lime','gun']
+empty_len = len(empty)
+print(empty_len)
