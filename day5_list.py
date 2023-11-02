d34dd2b90181eb27d347998ed3ed424ed6ff2fed
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-02 02:51:26 +0100

    finished day5

--- day5_list.py
+++ day5_list.py
@@ -1,3 +1,5 @@
+import statistics
+
 fruits = ['banana', 'orange', 'mango', 'lemon']
 all_fruits = fruits[0:4] # it returns all the fruits
 # this will also give the same result as the one above
@@ -119,10 +121,284 @@ ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 age_sort = ages.sort()
 # step - 2 find the min
 min_age = min(ages)
+print(min_age)
 max_age = max(ages)
+print(max_age)
 ages.append(min_age)
 ages.append(max_age)
 ages.sort()
-print(ages)
+
+# find the median age 
+mid_age_index= len(ages) // 2
+mid_age = ages[mid_age_index]
+# or 
+
+n = len(ages)
+
+if n % 2 == 0:
+    middle_1 = ages[n //2 -1]
+    middle_2 = ages[n //2]
+    median = (middle_1 + middle_2) / 2
+else:
+    median = ages[n // 2]
+
+print(round(median))
+
+# find average
+average = statistics.mean(ages)
+print(average)
 
 
+# or another way to find the average 
+xa = sum(ages)
+x_len = len(ages)
+average_2 = xa / x_len
+
+print(average_2)
+
+# find the age range
+
+range_age = range(max_age - min_age)
+print(range_age)
+
+compa_1 = abs(min_age - average)
+compa_2 = abs(max_age - average)
+print(compa_1)
+print(compa_2)
+
+if compa_1 == compa_2:
+    print("True")
+else:
+    print("False")
+
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+]
+
+
+ilu_index = len(countries) // 2
+ilu_string_value = countries[ilu_index]
+print(ilu_string_value)
+
+# or
+
+k = len(countries)
+
+if k % 2 == 0:
+    ilu_1 = countries[k // 2 -1]
+    ilu_2 =  countries[k //2]
+    calc_ilu = (ilu_1 + ilu_2) / 2
+    
+else:
+    calc_ilu = countries[k // 2]
+
+print(calc_ilu)
+
+midpoint = len(countries) // 2
+
+first_half = countries[:midpoint + 1]
+second_half = countries[midpoint: - 1]
+print(len(first_half))
+print(len(second_half))
+
+# using slice method to unpack a list
+west = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+first_west = west[0:3:]
+# scandic_countries = west[3::]
+
+
+# Unpacking a list 
+ch,ru,us,*scandic_countries = west
+print(scandic_countries)
