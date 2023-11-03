ef505343e799bfc740b54867e259dbe9eee6ec89
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-03 22:31:43 +0100

    finish tut

--- day_Tuple_6.py
+++ day_Tuple_6.py
@@ -7,14 +7,13 @@
 # count(): to count the number of a specified item in a tuple
 # index(): to find the index of a specified item in a tuple
 # operator: to join two or more tuples and to create a new tuple
-
 mars = ("age", 50, 0.5,"50")
 chan_mars = list(mars)
 chan_mars[0] = "messi"
-new_mars= tuple(chan_mars)
+new_mars = tuple(chan_mars)
 
 
-if "50" in new_mars:
+if 50 in new_mars:
     print("true")
 else:
     print("False")
@@ -41,6 +40,9 @@ print(dad)
 food_stuff_tp = ("Apple", "Mango", "Avocado","lettuce", "beef","chicken","bread")
 food_stuff_lt = list(food_stuff_tp)
 
+# FIND THE THE MIDDLE ITEM IN THE LIST 
+
+
 mid_tp = len(food_stuff_lt) // 2 
 mid_tp_index_val = food_stuff_lt[mid_tp]
 
@@ -49,19 +51,35 @@ n = len(food_stuff_lt)
 
 # if the middle item is an even number then calculate both figures and then divide by 2
 if n % 2 == 0:
-    # find the first middle item in the list
+    #find the first middle item in the list
     mid1 = food_stuff_lt[n // 2 -1]
-    # find the second middle item in the list
+    #find the second middle item in the list
     mid2 = food_stuff_lt[n // 2]
-    # add the two items together and then divide by to find the middle item
+    #add the two items together and then divide by to find the middle item
     midpoint = (mid1 + mid2) / 2
     print(midpoint)
 
-    # if the item is an odd number then print the item
+    #if the item is an odd number then print the item
 else:
     mid3 = food_stuff_lt[n // 2]
     print(mid3)
 
 
+slice_lt =  food_stuff_lt[:3:]
+print(slice_lt)
 
+del food_stuff_lt
 
+nordic_countries = ("Denmark", "Finland","Iceland", "Norway", "Sweden")
+
+if "Estonia" in nordic_countries:
+
+    print("Nordic Country")
+else: 
+    print("Not a Nordic Country")
+    
+
+if "Iceland" in nordic_countries:
+    print("Nordic Country")
+else: 
+    print("Not Nordic Country")
