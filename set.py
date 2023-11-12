11a96c5b50ec61c78290734773b6f6cff8ede593
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-12 23:05:14 +0100

    More

--- set.py
+++ set.py
@@ -10,7 +10,16 @@
 st = {'item1', 'item2', 'item3', 'item4'}
 st.update(['item5','item6','item7'])
 fruits = {'banana', 'orange', 'mango', 'lemon'}
-vegetables = ('tomato', 'potato', 'cabbage','onion', 'carrot')
+vegetables = ['tomato', 'potato', 'cabbage','onion', 'carrot']
 fruits.update(vegetables)
-
 print(fruits)
+print(type(fruits))
+
+remove_fruit= fruits.pop
+print(remove_fruit)
+
+cool_number = 12 + 30
+ak = cool_number * 5
+print(cool_number)
+
+print(ak)
