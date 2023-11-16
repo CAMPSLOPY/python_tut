a6eda0393f2cbbc635b927e1895390dfdfee330e
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-16 16:44:40 +0100

    conditional statement excercise

---
+++ condit.py
@@ -0,0 +1,4 @@
+# Conditional execution: a block of one or more statements will be executed if a certain expression is true
+
+# Repetitive execution: a block of one or more statements will be repetitively executed as long as a certain expression is true. In this section, we will cover if, else, elif statements. 
+# The comparison and logical operators we learned in previous sections will be useful here.
