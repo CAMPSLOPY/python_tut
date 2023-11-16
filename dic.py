f17627df1ad2781d6bf31a4eb34dce8ac3307c2c
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-16 02:57:42 +0100

    dictionary python tut

---
+++ dic.py
@@ -0,0 +1,14 @@
+# A dictionary is a collection of unordered, modifiable(mutable) paired (key: value) data type.
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
