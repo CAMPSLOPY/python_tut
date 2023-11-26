de96fee94e31b765d48183cbc9516342e2ee03f6
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-26 22:31:03 +0100

    loopy

--- loops.py
+++ loops.py
@@ -22,7 +22,37 @@ for key in person:
 for n in range(0, 11):
     print(n)
 
-count = 0
-while count < 11:
-    print(count)
-    count = count + 1
+# count = 0
+# while count < 11:
+#     print(count)
+#     count = count + 1
+
+
+for n in range(1, 8):
+    if n <= 8:
+        print(" # " * 8)
+
+
+# for n in range(11, 0, -1):
+#     print(n)
+
+# count = 11
+# while count > -1:
+#     print(count)
+#     count = count - 1
+
+for n in range(0, 11):
+    print(n * n)
+
+
+
+
+IDE =  ['Python', 'Numpy','Pandas','Django', 'Flask']
+
+for i in IDE:
+    print(i)
+
+# Iterate 0 - 100  and print only even numbers
+
+#  if n % 2 == 0:
+# print 
