63235175ef22599413e56dec3320a6304f0f273c
CAMPSLOPY <tola.shobowale@fefundinfo.com>
2023-11-26 23:14:37 +0100

    loopy

--- loops.py
+++ loops.py
@@ -19,8 +19,8 @@ for key in person:
 
 
 
-for n in range(0, 11):
-    print(n)
+# for n in range(0, 11):
+#     print(n)
 
 # count = 0
 # while count < 11:
@@ -28,9 +28,9 @@ for n in range(0, 11):
 #     count = count + 1
 
 
-for n in range(1, 8):
-    if n <= 8:
-        print(" # " * 8)
+# for n in range(1, 8):
+#     if n <= 8:
+#         print(" # " * 8)
 
 
 # for n in range(11, 0, -1):
@@ -41,18 +41,27 @@ for n in range(1, 8):
 #     print(count)
 #     count = count - 1
 
-for n in range(0, 11):
-    print(n * n)
+# for n in range(0, 11):
+#     print(n * n)
 
 
 
 
-IDE =  ['Python', 'Numpy','Pandas','Django', 'Flask']
+# IDE =  ['Python', 'Numpy','Pandas','Django', 'Flask']
 
-for i in IDE:
-    print(i)
+# for i in IDE:
+#     print(i)
 
 # Iterate 0 - 100  and print only even numbers
 
 #  if n % 2 == 0:
-# print 
+# print even numbers
+
+for n in range(0,101):
+    if n % 2 == 0:
+        addit = []
+        print(n, "is an even number in the loop")
+
+for x in range(0, 100):
+    if x % 2:
+        print(x, "is an odd number in the loop")
